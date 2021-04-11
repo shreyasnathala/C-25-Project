5c5871b6da9d10b472a31de59409a146c628baa3
shreyasnathala <70395653+shreyasnathala@users.noreply.github.com>
2021-04-11 13:53:36 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,32 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+			}
+		this.r=r;
+		this.body=Bodies.circle(x, y,r, options);
+        this.image=loadImage("paper.png")
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+		image(this.image,0,0,this.r,this.r)
+			pop()
+			
+	}
+
+}
